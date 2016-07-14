7cfedba7443814935cf3b0c27a9fd6fadd495cac
unknown <barryjus@OJPSRAP51759.ojpsranet.gov>
2016-07-14 17:18:53 -0400

    more practice

---
+++ src/java/cert/practice/Numbers.java
@@ -0,0 +1,23 @@
+package java.cert.practice;
+
+public class Numbers {
+	public static void main(String[] args) {
+		Byte b = 0; // 8-bit
+		Short s = 0; // 16-bi
+		Integer i = 0; // 32-bit
+		Long l = 0L; // 64-bit
+		Float f = 0.0f; // single precision 32-bit floating point
+		Double d = 0.0; // double precision 64-bit floating point
+		
+		byte bValue = b.byteValue();
+		
+		Byte b2 = new Byte(bValue);
+		
+		boolean equalTo = b.equals(b2);
+		
+		System.out.println("Equals?" + equalTo);
+		
+		int parsedInteger = Integer.parseInt("102");
+		System.out.println("pased integer value: " + parsedInteger);
+	}
+}
---
+++ src/java/cert/practice/PrimitiveDataTypes.java
@@ -0,0 +1,16 @@
+package java.cert.practice;
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		byte b = 0; // 8-bit
+		short s = 0; // 16-bi
+		int i = 0; // 32-bit
+		long l = 0L; // 64-bit
+		float f = 0.0f; // single precision 32-bit floating point
+		double d = 0.0; // double precision 64-bit floating point
+		boolean bool = false;
+		char c = 0; // single 16-bit Unicode character 0 <= char <= 65,535
+		char aChar = 'H'; // single quotes for character
+		String str = "Hello World!";
+	}
+}
