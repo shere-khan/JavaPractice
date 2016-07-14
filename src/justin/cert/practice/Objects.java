cd8d8973c36406ff4b8cdfc4ffb7278677ba4ebb
unknown <barryjus@OJPSRAP51759.ojpsranet.gov>
2016-07-14 17:58:55 -0400

    new classes

---
+++ src/justin/cert/practice/Objects.java
@@ -0,0 +1,9 @@
+package justin.cert.practice;
+
+public class Objects {
+	public static void main(String[] args) {
+		Bicycle bike = new Bicycle(10, 0, 1);
+		
+		System.out.println(bike.getCadence());
+	}
+}
---
+++ src/justin/cert/practice/Strings.java
@@ -0,0 +1,26 @@
+package justin.cert.practice;
+
+public class Strings {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder();
+		
+		System.out.println("StringBuilder length: " + str.capacity());
+		
+		System.out.println("\nString: " + str);
+		
+		str.append("Hello");
+		str.append(' ');
+		str.append(123);
+		str.append(" World!");
+		
+		System.out.println("\nString: " + str);
+		
+		str.insert(1, 'x');
+		str.insert(3, 'y');
+		str.insert(5, 'z');
+		
+		System.out.println("\nString: " + str);
+		
+		str.replace(1, 6, "a b c");
+	}
+}
