f4c148f605be61c20e582bd695aa4622e444117d
unknown <barryjus@OJPSRAP51759.ojpsranet.gov>
2016-07-14 16:46:17 -0400

    changed to Java 7

---
+++ src/java/cert/practice/PracticeVariables.java
@@ -0,0 +1,30 @@
+package java.cert.practice;
+
+public class PracticeVariables {
+	
+	static int staticField;
+	
+	static {
+		staticField = 20;
+	}
+
+	static int anotherStaticField = initialize();
+	
+	static int initialize() {
+		return staticField * 2;
+	}
+	
+	int myValue = 10;
+
+	int anotherValue;
+	
+	{
+		anotherValue = myValue * 3;
+	}
+
+	int myValue2 = initValue();
+	
+	final int initValue() {
+		return anotherValue + 10;
+	}
+}
